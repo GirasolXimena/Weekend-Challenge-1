3fc8a762cad2535cbe5fb6c18c4cd5841b7ae4e6
Robert Andrade <srandradejr@gmail.com>
2018-05-19 17:01:04 -0500

    finished base mode
    all base mode functionality working as expected

--- Scripts/client.js
+++ Scripts/client.js
@@ -5,23 +5,15 @@ function onReady() {
     console.log('JQ');
 
     $(`#submitButton`).on( `click`, function() {
-        ;
-    });
-}
+        addEmployee();
+        checkCost();
+        
+          }
 
-   
-//     //    $(`#fNameInput`).val(),
-//     //    $(`#lNameInput`).val(),
-//     //    $(`#idInput`).val(),
-//     //    $(`#titleInput`).val(),
-//     //    $(`#salaryInput`).val(),
-//    )
-   
-   
-//    });
-// };
-
+    )};
 let workers = [];
+let cost = [];
+let totalCosts =0;
 class Employee {
     constructor (firstName, lastName, idNum, jTitle, annualSalary)  {
         this.firstName = firstName;
@@ -33,26 +25,53 @@ class Employee {
 }
 
 function addEmployee( firstName, lastName, idNum, jTitle, annualSalary) {
-    
-    
+    $(`#outputString`).empty();
     firstName = $(`#fNameInput`).val();
     lastName =$(`#lNameInput`).val();
     idNum = $(`#idInput`).val();
     jTitle = $(`#titleInput`).val();
     annualSalary = $(`#salaryInput`).val();
-    
-    
-    
     let newEmployee = new Employee ( firstName, lastName, idNum, jTitle, annualSalary);
     workers.push(newEmployee);
     console.log(workers);
 
+    for (employee of workers) {
+        
+        let outPut = `<div class="row">`;
+        outPut += `<div class="col-2">`;
+        outPut += `<p>` + employee.firstName + `</p>`;
+        outPut += `</div>`;
+        outPut += `<div class="col-2">`;
+        outPut += `<p>` + employee.lastName + `</p>`;
+        outPut += `</div>`;        
+        outPut += `<div class="col-2">`;
+        outPut += `<p>` + employee.idNum + `</p>`;
+        outPut += `</div>`;
+        outPut += `<div class="col-2">`;
+        outPut += `<p>` + employee.jTitle + `</p>`;
+        outPut += `</div>`;
+        outPut += `<div class="col-2">`;
+        outPut += `<p>` + employee.annualSalary + `</p>`;
+        outPut += `</div>`;
+        $(`#outputString`).append(outPut);
+        
+    }
+    $(`#costOutput`).empty();
+    newCost = parseInt($(`#salaryInput`).val());
+    totalCosts += newCost;
+    $(`#costOutput`).append(`Total Monthly Costs: $` + totalCosts);
+
     firstName = $(`#fNameInput`).val(``);
-    lastName =$(`#lNameInput`).val(``);
-    idNum = $(`#idInput`).val(``);
-    jTitle = $(`#titleInput`).val(``);
-    annualSalary = $(`#salaryInput`).val(``);
-       
+lastName =$(`#lNameInput`).val(``);
+idNum = $(`#idInput`).val(``);
+jTitle = $(`#titleInput`).val(``);
+annualSalary = $(`#salaryInput`).val(``);
 }
 
-// addEmployee(`bob`,`butt`,69,`buttmaster`,420);
+function checkCost() {
+    if (totalCosts >= 20000) {
+    console.log('in checkCost');
+    $(`#costOutput`).addClass(`redBackground`);
+    }
+}
+       
