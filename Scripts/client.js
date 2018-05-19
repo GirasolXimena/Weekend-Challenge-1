5213ea0adb246a0b9dc19fc95b7f39bce50fa28d
Robert Andrade <srandradejr@gmail.com>
2018-05-19 13:51:39 -0500

    got constructors and push to array working from user input

--- Scripts/client.js
+++ Scripts/client.js
@@ -3,6 +3,56 @@ console.log('JS');
 
 function onReady() {
     console.log('JQ');
-    
-    
+
+    $(`#submitButton`).on( `click`, function() {
+        ;
+    });
 }
+
+   
+//     //    $(`#fNameInput`).val(),
+//     //    $(`#lNameInput`).val(),
+//     //    $(`#idInput`).val(),
+//     //    $(`#titleInput`).val(),
+//     //    $(`#salaryInput`).val(),
+//    )
+   
+   
+//    });
+// };
+
+let workers = [];
+class Employee {
+    constructor (firstName, lastName, idNum, jTitle, annualSalary)  {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNum = idNum;
+        this.jTitle = jTitle;
+        this.annualSalary = annualSalary;
+    }
+}
+
+function addEmployee( firstName, lastName, idNum, jTitle, annualSalary) {
+    
+    
+    firstName = $(`#fNameInput`).val();
+    lastName =$(`#lNameInput`).val();
+    idNum = $(`#idInput`).val();
+    jTitle = $(`#titleInput`).val();
+    annualSalary = $(`#salaryInput`).val();
+    
+    
+    
+    let newEmployee = new Employee ( firstName, lastName, idNum, jTitle, annualSalary);
+    workers.push(newEmployee);
+    console.log(workers);
+
+    firstName = $(`#fNameInput`).val(``);
+    lastName =$(`#lNameInput`).val(``);
+    idNum = $(`#idInput`).val(``);
+    jTitle = $(`#titleInput`).val(``);
+    annualSalary = $(`#salaryInput`).val(``);
+       
+}
+
+// addEmployee(`bob`,`butt`,69,`buttmaster`,420);
