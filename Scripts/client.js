a4689766b9df09309bd9c2be1727ae098248b519
Robert Andrade <srandradejr@gmail.com>
2018-05-20 22:08:47 -0500

    final push
    finished all functionality. Gave up on css

--- Scripts/client.js
+++ Scripts/client.js
@@ -3,7 +3,7 @@ console.log('JS');
 
 function onReady() {
     console.log('JQ');
-
+    $(`#rmEmployeeButton`).on( `click`, removeEmployee)
     $(`#submitButton`).on( `click`, function() {
         addEmployee();
         checkCost();
@@ -25,20 +25,25 @@ class Employee {
 }
 
 function addEmployee( firstName, lastName, idNum, jTitle, annualSalary) {
-    $(`#outputString`).empty();
     firstName = $(`#fNameInput`).val();
     lastName =$(`#lNameInput`).val();
     idNum = $(`#idInput`).val();
     jTitle = $(`#titleInput`).val();
     annualSalary = $(`#salaryInput`).val();
+    if( firstName === "" || lastName === "" || idNum === "" || jTitle === "" || 
+    annualSalary === "") {
+        alert (`Please fill in all boxes before continuing.`);
+    }
+    else {
+    $(`#outputString`).empty();
     let newEmployee = new Employee ( firstName, lastName, idNum, jTitle, annualSalary);
     workers.push(newEmployee);
     console.log(workers);
-
+    let el = 0;
     for (employee of workers) {
-        
-        let outPut = `<div class="row">`;
-        outPut += `<div class="col-2">`;
+        el++;
+        let outPut = `<div class="row card-body" id="` + employee.firstName + `">`;
+        outPut += `<div class="col-2" >`;
         outPut += `<p>` + employee.firstName + `</p>`;
         outPut += `</div>`;
         outPut += `<div class="col-2">`;
@@ -60,13 +65,15 @@ function addEmployee( firstName, lastName, idNum, jTitle, annualSalary) {
     newCost = parseInt($(`#salaryInput`).val());
     totalCosts += newCost;
     $(`#costOutput`).append(`Total Monthly Costs: $` + totalCosts);
+    newCost=0;
 
     firstName = $(`#fNameInput`).val(``);
 lastName =$(`#lNameInput`).val(``);
 idNum = $(`#idInput`).val(``);
 jTitle = $(`#titleInput`).val(``);
 annualSalary = $(`#salaryInput`).val(``);
-}
+return totalCosts;
+}}
 
 function checkCost() {
     if (totalCosts >= 20000) {
@@ -74,4 +81,18 @@ function checkCost() {
     $(`#costOutput`).addClass(`redBackground`);
     }
 }
-       
+
+function removeEmployee() {
+    
+    $(`#` + workers[workers.length- 1].firstName).empty();
+    $(`#costOutput`).empty();
+    newCost = parseInt((workers[workers.length- 1].annualSalary));
+    console.log(newCost);
+    totalCosts -= newCost;
+    console.log(totalCosts);
+    $(`#costOutput`).append(`Total Monthly Costs: $` + totalCosts);
+    workers.pop();
+    newCost=0;
+    return totalCosts;
+
+}
